cc438787c72cb65b6226cc76e9074029937ed301
Iona Carslaw <ionacarslaw@Ionas-MacBook-Pro-2.local>
2022-09-08 14:13:06 +0100

    finished tmx function

--- preprocessing_scripts/clean_format.py
+++ preprocessing_scripts/clean_format.py
@@ -16,15 +16,19 @@ def open_file(file_path):
 
 #change to TMX format
 def turn_into_tmx(en_lst, cor_lst, file_name):
+    #gets current time
     current_time = time.ctime(time.time())
     times_five_tab = "\t" * 5
     tmx_file = open(f"{file_name}.tmx", "a+")
     #add header elements to tmx file
     tmx_file.write("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" + "\n" + "<tmx version=\"1.4\">" + "\n")
-    tmx_file.write("<header creationdate=" + str(current_time) + "\n" + times_five_tab + "srclang=\"en\"" + "\n" + times_five_tab + "adminlang=\"en\"" + "\n" + times_five_tab + "o-tmf=\"unknown\"" + "\n" + times_five_tab + "segtype=\"sentence\"" + "\n" + times_five_tab + "creationtool=\"Uplug\"" + "\n" + times_five_tab + "creationtoolversion=\"unknown\"" + "\n" + times_five_tab + "datatype=\"PlainText\" />")
+    tmx_file.write("<header creationdate=" + str(current_time) + "\n" + times_five_tab + "srclang=\"en\"" + "\n" + times_five_tab + "adminlang=\"en\"" + "\n" + times_five_tab + "o-tmf=\"unknown\"" + "\n" + times_five_tab + "segtype=\"sentence\"" + "\n" + times_five_tab + "creationtool=\"Uplug\"" + "\n" + times_five_tab + "creationtoolversion=\"unknown\"" + "\n" + times_five_tab + "datatype=\"PlainText\" />" + "\n")
+    tmx_file.write("  <body>" + "\n")
+    #iterates through cornish and english lists and write to file with tmx format
+    for i in range(len(cor_lst)):
+        tmx_file.write("    <tu>" + "\n" + f"      <tuv xml:lang=\"en\"><seg>{en_lst[i]} </seg></tuv>" + "\n" + f"      <tuv xml:lang=\"kw\"><seg>{cor_lst[i]} </seg></tuv>" + "\n" + "    </tu>" + "\n")
+    tmx_file.write("  </body>" +  "\n" + "</tmx>")
     tmx_file.close()
-
-
 
 
 
@@ -32,5 +36,5 @@ if __name__ == "__main__":
     #opens cornish and english raw_dataset file
     cornish_file = open_file('/Users/ionacarslaw/Desktop/Cor-En/raw_datasets/1_COR.txt')
     english_file = open_file('/Users/ionacarslaw/Desktop/Cor-En/raw_datasets/1_ENG.txt')
-    turn_into_tmx(cornish_file, english_file, "test")
+    turn_into_tmx(english_file, cornish_file, "test")
     print("FINISH")
