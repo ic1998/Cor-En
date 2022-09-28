a7e5a4cbcec1d9ca55a3223911463fc334aceabd
Iona Carslaw <ionacarslaw@Ionas-MacBook-Pro-2.local>
2022-09-28 13:39:32 +0100

    initial loop to extract different text for dict

--- preprocessing_scripts/pdf_convertion.py
+++ preprocessing_scripts/pdf_convertion.py
@@ -10,7 +10,19 @@ abbrevs = ["abbr", "adj", "adv", "art", "cnj", "contr", "int", "n", "n.coll", "n
 "prfx", "prn", "prp", "sffx", "top", "vb"]
 
 #opens pdf and reads it
+"""
 with pdfplumber.open("./cornish_dict.pdf") as pdf:
-    text = pdf.pages[20]
+    text = pdf.pages[5]
     clean_text = text.filter(lambda obj: obj["object_type"] == "char" and "Bold" in obj["fontname"])
     print(clean_text.extract_text())
+"""
+
+with pdfplumber.open("./cornish_dict.pdf") as pdf:
+    for page in pdf.pages:
+        if page == pdf.pages[6]:
+            text = page.extract_text()
+            bold_text = page.filter(lambda obj: obj["object_type"] == "char" and "Bold" in obj["fontname"])
+            for line in text.split('\n'):
+                print(line)
+            for line in bold_text.split('\n'):
+                print(line)
